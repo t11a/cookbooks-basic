e472fbb8325cf53a3748e7b288a98202f35ae067
Ogawa <tsuyoga@34363b776a12.ant.amazon.com>
2015-03-24 18:41:40 +0900

    add zsh recipe

---
+++ zsh/recipes/default.rb
@@ -0,0 +1,27 @@
+package "zsh" do
+  action :install
+end
+
+username = 'ec2-user'
+
+git "/home/#{username}/.oh-my-zsh" do
+  repository  "git://github.com/robbyrussell/oh-my-zsh.git"
+  reference   "master"
+  action      :checkout
+  user        username
+  group       username
+end
+
+=begin
+template "/home/#{username}/.zshrc" do
+  path    "/home/#{username}/.zshrc"
+  source  "zshrc.erb"
+  owner   username
+  group   username
+  mode    0644
+end
+=end
+
+execute "set zsh as default shell" do
+  command "chsh -s $(which zsh) #{username}"
+end
